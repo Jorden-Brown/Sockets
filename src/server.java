26f0ad754add1173d334fc9a9174aa8af0d2d171
Jorden-Brown <49078626+Jorden-Brown@users.noreply.github.com>
2022-07-11 01:44:41 -0400

    second commit upload

--- src/server.java
+++ src/server.java
@@ -6,14 +6,21 @@ public class server {
 	public static void main(String[] args) throws IOException{
 		ServerSocket ss = new ServerSocket(4999);
 		Socket s = ss.accept();
-		
+		//welcome message when the server is spun up 
 		System.out.println("client connected Welcome to the VOID");
 
 		InputStreamReader  in = new InputStreamReader(s.getInputStream());
 		BufferedReader bf = new BufferedReader(in);
 		
+		
 		String str = bf.readLine();
 		System.out.println("Client: "+ str);
+		
+		PrintWriter pr = new PrintWriter(s.getOutputStream());
+		pr.println("sugoi, it does");
+		pr.flush();
+		
+		
 	}
 
 }
--- src/socket.java
+++ src/socket.java
@@ -4,11 +4,21 @@ import java.io.*;
 public class socket {
 
 	public static void main(String[] args) throws IOException{
+		
 		Socket s = new Socket("localhost", 4999);
-
+		
 		PrintWriter pr = new PrintWriter(s.getOutputStream());
-		pr.println("Subarashi");
+		pr.println("Subarashi, does it work??");
 		pr.flush();
+		
+		
+		InputStreamReader  in = new InputStreamReader(s.getInputStream());
+		BufferedReader bf = new BufferedReader(in);
+		
+		
+		String str = bf.readLine();
+		System.out.println("server: "+ str);
+		
 	}
 
 }
